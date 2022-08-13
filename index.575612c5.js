328cfc5ca80f5a0b50f4384432b54dc108714a44
Bzezinskij <110726032+Bzezinskij@users.noreply.github.com>
2022-08-13 16:08:39 +0000

    Deploying to gh-pages from @ bzhezinskyi/IceCream-team2@1127a6ed8e76fdf78100a50bc77b5400e30f6704 🚀

---
+++ index.575612c5.js
@@ -0,0 +1,2 @@
+mybutton=document.getElementById("myBtn"),window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"};
+//# sourceMappingURL=index.575612c5.js.map
---
+++ index.7da3f063.js
@@ -0,0 +1,2 @@
+mybutton=document.getElementById("myBtn"),window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"};
+//# sourceMappingURL=index.7da3f063.js.map
