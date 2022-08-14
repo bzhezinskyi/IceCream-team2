e3401f10c2e7283c3425b1dbb62ce6bcea8f97a9
Kastasov <kastasov.d@gmail.com>
2022-08-14 23:28:05 +0300

    pic array + counter
    добавил соурссет картинок  в хиро
    прикрутил счетчик к хиро и цифре молока в приимуществах, не знаю как запускать счетчик после момента видимости значения

---
+++ src/js/counter.js
@@ -0,0 +1,50 @@
+const time = 10000; // ms - hero-cafe
+const step = 1; 
+const time2 = 20000; // ms hero-track
+const step2 = 1; 
+const time3 = 50000; // ms advantages -milk
+const step3 = 1; 
+
+function outNum(num, elem) {
+	let l = document.querySelector('#' + elem);
+	n = 0;
+	let t = Math.round(time/(num/step));
+	let interval = setInterval(() => {
+		n = n + step;
+		if (n == num) {
+			clearInterval(interval);
+		}
+		l.innerHTML = n;
+	},
+		t);
+}
+function outNum2(num2, elem2) {
+	let l = document.querySelector('#' + elem2);
+	a = 0;
+	let s = Math.round(time2/(num2/step2));
+	let interval = setInterval(() => {
+		a = a + step2;
+		if (a == num2) {
+			clearInterval(interval);
+		}
+		l.innerHTML = a;
+	},
+		s);
+}
+function outNum3(num3, elem3) {
+	let l = document.querySelector('#' + elem3);
+	b = 0;
+	let f = Math.round(time3/(num3/step3));
+	let interval = setInterval(() => {
+		b = b + step3;
+		if (b == num3) {
+			clearInterval(interval);
+		}
+		l.innerHTML = b;
+	},
+		f);
+}
+
+outNum(16, 'cafe-count');
+outNum2(50, 'track-count');
+outNum3(726, 'milk-count');
