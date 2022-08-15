ff17bfcebe2ef0e69764d90742b4db1a34c18fad
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-08-15 09:40:05 +0000

    Deploying to gh-pages from @ bzhezinskyi/IceCream-team2@263299dea86a7167848dacf8fa21ed728619a986 🚀

---
+++ index.e053e7b7.js
@@ -0,0 +1,2 @@
+!function(e,r){var t=document.querySelector("#"+r);n=0;var a=Math.round(1e4/(e/1)),c=setInterval((function(){n+=1,n==e&&clearInterval(c),t.innerHTML=n}),a)}(16,"cafe-count"),function(n,e){var r=document.querySelector("#"+e);a=0;var t=Math.round(2e4/(n/1)),c=setInterval((function(){a+=1,a==n&&clearInterval(c),r.innerHTML=a}),t)}(50,"track-count"),function(n,e){var r=document.querySelector("#"+e);b=0;var t=Math.round(5e4/(n/1)),a=setInterval((function(){b+=1,b==n&&clearInterval(a),r.innerHTML=b}),t)}(726,"milk-count");
+//# sourceMappingURL=index.e053e7b7.js.map
