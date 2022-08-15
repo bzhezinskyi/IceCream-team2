26a62c2d11e7fcf10d2791213d9c804da1c39170
Bzezinskij <110726032+Bzezinskij@users.noreply.github.com>
2022-08-15 13:47:45 +0000

    Deploying to gh-pages from @ bzhezinskyi/IceCream-team2@98256d0ac23a3b0349752b3f1a07b7c5eba4cfa1 🚀

---
+++ index.98065425.js
@@ -0,0 +1,2 @@
+var o=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"300")}));
+//# sourceMappingURL=index.98065425.js.map
