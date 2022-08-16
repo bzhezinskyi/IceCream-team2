2e9d5d567251dfaae51a6cf2695c63fc16aafc5c
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2022-08-16 16:39:41 +0300

    add SimpleLightbox preview to our gallery

---
+++ src/js/gallery.js
@@ -0,0 +1,8 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+var lightbox = new SimpleLightbox('.gallery-ice a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
+// npm install simplelightbox
